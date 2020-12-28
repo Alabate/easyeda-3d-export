35e18166a7fc5c0d396b7a01d552c9e51c7bc6f1
Aurelien Labate <aurelien@labate.me>
2020-12-28 16:19:43 +0100

    Replace arc with string if it cannot be drawn

--- DataProcess.js
+++ DataProcess.js
@@ -35,77 +35,86 @@ export function getBoardOutlinePolygones(data) {
           (p) => new jsts.geom.Coordinate(p.x, p.y)
         );
 
-        // Only support circles
-        if (shape.radiusX != shape.radiusY) {
-          throw new Error("Ellipsis arcs not supported. Only circle arcs are.");
+        let lineString;
+        try {
+          // Only support circles
+          if (shape.radiusX != shape.radiusY) {
+            throw new Error(
+              "Ellipsis arcs not supported. Only circle arcs are."
+            );
+          }
+
+          // Doc to find position of center from points and radius
+          // https://www.overleaf.com/read/stpqdczjqsdz
+
+          // Set initial values
+          const r = shape.radiusX;
+          const x1 = shape.points[0].x;
+          const y1 = shape.points[0].y;
+          const x2 = shape.points[1].x;
+          const y2 = shape.points[1].y;
+
+          // Set intermediate values
+          // Dist between P1 and P2
+          const dist_p1_p2 = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+          // Unit vector of the line that pass by both circle centers (M line)
+          const mx = (y1 - y2) / dist_p1_p2;
+          const my = (x2 - x1) / dist_p1_p2;
+          // P3, Point on the M line, and in the middle of P1 and P2
+          const x3 = (x1 + x2) / 2;
+          const y3 = (y1 + y2) / 2;
+
+          // Dist between P3 and circle center C1
+          let tmp = r ** 2 - (x3 - x1) ** 2 - (y3 - y1) ** 2;
+          // When P3 = C1 = C2, tmp might be equal to a very small negative value like -7.105427357601002e-15
+          // Which will cause a NaN once we applied sqrt to it
+          // This is probably because of floating point approx
+          // So we ignore values under nanometer
+          const epsilon = 10 ** -6;
+          if (Math.abs(tmp) < epsilon) {
+            tmp = 0;
+          }
+          const dist_p3_c = Math.sqrt(tmp);
+
+          // Computer circle center
+          let cx, cy;
+          if (
+            (shape.clockwise && shape.solution_selection) ||
+            (!shape.clockwise && !shape.solution_selection)
+          ) {
+            cx = x3 + dist_p3_c * mx;
+            cy = y3 + dist_p3_c * my;
+          } else {
+            cx = x3 - dist_p3_c * mx;
+            cy = y3 - dist_p3_c * my;
+          }
+
+          // Compute arc begin and end in polar coordinates
+          let theta1 = Math.atan2(y2 - cy, x2 - cx);
+          let theta2 = Math.atan2(y1 - cy, x1 - cx);
+
+          // If not clockwise, make it clockwise
+          if (!shape.clockwise) {
+            [theta1, theta2] = [theta2, theta1];
+          }
+
+          // Compute angle size in rad
+          while (theta2 < theta1) {
+            theta2 += 2 * Math.PI;
+          }
+          const angleSize = (theta2 - theta1) % (2 * Math.PI);
+
+          // Create the shape
+          const geometricShapeFactory = new jsts.util.GeometricShapeFactory();
+          geometricShapeFactory.setWidth(r * 2);
+          geometricShapeFactory.setHeight(r * 2);
+          geometricShapeFactory.setCentre({ x: cx, y: cy });
+          lineString = geometricShapeFactory.createArc(theta1, angleSize);
+        } catch (error) {
+          // On any error, fallback to a simple line
+          console.warn("Fail to draw arc", error, shape);
+          lineString = geometryFactory.createLineString(coords);
         }
-
-        // Doc to find position of center from points and radius
-        // https://www.overleaf.com/read/stpqdczjqsdz
-
-        // Set initial values
-        const r = shape.radiusX;
-        const x1 = shape.points[0].x;
-        const y1 = shape.points[0].y;
-        const x2 = shape.points[1].x;
-        const y2 = shape.points[1].y;
-
-        // Set intermediate values
-        // Dist between P1 and P2
-        const dist_p1_p2 = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-        // Unit vector of the line that pass by both circle centers (M line)
-        const mx = (y1 - y2) / dist_p1_p2;
-        const my = (x2 - x1) / dist_p1_p2;
-        // P3, Point on the M line, and in the middle of P1 and P2
-        const x3 = (x1 + x2) / 2;
-        const y3 = (y1 + y2) / 2;
-
-        // Dist between P3 and circle center C1
-        let tmp = r ** 2 - (x3 - x1) ** 2 - (y3 - y1) ** 2;
-        // When P3 = C1 = C2, tmp might be equal to a very small negative value like -7.105427357601002e-15
-        // Which will cause a NaN once we applied sqrt to it
-        // This is probably because of floating point approx
-        // So we ignore values under nanometer
-        const epsilon = 10 ** -6;
-        if (Math.abs(tmp) < epsilon) {
-          tmp = 0;
-        }
-        const dist_p3_c = Math.sqrt(tmp);
-
-        // Computer circle center
-        let cx, cy;
-        if (
-          (shape.clockwise && shape.solution_selection) ||
-          (!shape.clockwise && !shape.solution_selection)
-        ) {
-          cx = x3 + dist_p3_c * mx;
-          cy = y3 + dist_p3_c * my;
-        } else {
-          cx = x3 - dist_p3_c * mx;
-          cy = y3 - dist_p3_c * my;
-        }
-
-        // Compute arc begin and end in polar coordinates
-        let theta1 = Math.atan2(y2 - cy, x2 - cx);
-        let theta2 = Math.atan2(y1 - cy, x1 - cx);
-
-        // If not clockwise, make it clockwise
-        if (!shape.clockwise) {
-          [theta1, theta2] = [theta2, theta1];
-        }
-
-        // Compute angle size in rad
-        while (theta2 < theta1) {
-          theta2 += 2 * Math.PI;
-        }
-        const angleSize = (theta2 - theta1) % (2 * Math.PI);
-
-        // Create the shape
-        const geometricShapeFactory = new jsts.util.GeometricShapeFactory();
-        geometricShapeFactory.setWidth(r * 2);
-        geometricShapeFactory.setHeight(r * 2);
-        geometricShapeFactory.setCentre({ x: cx, y: cy });
-        let lineString = geometricShapeFactory.createArc(theta1, angleSize);
 
         // To try reproduce EasyEDA behaviour, the outline has always a width of 0.3mm
         polygons.push(lineString.buffer(OUTLINE_WIDTH / 2));
